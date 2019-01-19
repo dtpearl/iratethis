bf6590f467b22a665f7e5e29c878e96c6f00fbca
David Pearl <davidtpearl@gmail.com>
2019-01-19 17:51:22 +1100

    Before attempt to seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u1 = User.create :name => "Jamie" :email => "jamie@ga.co" :password => "chicken"
+u2 = User.create :name => "Gordo" :email => "gordo@ga.co" :password => "chicken"
+u3 = User.create :name => "Bobby" :email => "bobby@ga.co" :password => "chicken"
+
+Post.destroy_all
+p1 = Post.create :post_text => "Yummo. Great breakfast" :post_image => "http://fillmurray.com/200/200"
+p2 = Post.create :post_text => "Yummo. Great brunch" :post_image => "http://fillmurray.com/200/200"
+p3 = Post.create :post_text => "Yummo. Great lunch" :post_image => "http://fillmurray.com/200/200"
+p4 = Post.create :post_text => "Yummo. Great scot" :post_image => "http://fillmurray.com/200/200"
+p5 = Post.create :post_text => "Yummo. Great dinner" :post_image => "http://fillmurray.com/200/200"
+p6 = Post.create :post_text => "Yummo. Great dessert" :post_image => "http://fillmurray.com/200/200"
+
+Restaurant.destroy_all
+r1 = Restaurant.create :restaurant_name => "McDonalds" :address => "1 McDonalds St, Sydney"
+r2 = Restaurant.create :restaurant_name => "Hungry Jacks" :address => "3 Hungry Jacks St, Sydney"
+r3 = Restaurant.create :restaurant_name => "Subway" :address => "5 Subway St, Sydney"
+r4 = Restaurant.create :restaurant_name => "KFC" :address => "7 Chicken St, Sydney"
+r5 = Restaurant.create :restaurant_name => "Pizza Hut" :address => "9 Pizza St, Sydney"
+
+Comment.destroy_all
+c1 = Comment.create :comment_text => "Looks like a yummy Big Mac"
+c2 = Comment.create :comment_text => "What a yummy Big Mac"
+c3 = Comment.create :comment_text => "Big Macs are the best!"
+c4 = Comment.create :comment_text => "The burgers ARE better."
+c5 = Comment.create :comment_text => "I can't belive I ate the whole thing"
+c6 = Comment.create :comment_text => "Finger lickin good!"
+c7 = Comment.create :comment_text => "Get in my belly"
+
+# Associations
+#######################################################
+
+# users and posts
+u1.posts << p1 << p2 << p3
+u2.posts << p4 << p5
+u3.posts << p6
+
+# posts and comments
+p1.comments << c5
+p3.comments << c1 << c2 << c3
+p2.comments << c4
+p4.comments << c6 << c7
+
+# posts and restaurants
+p1.restaurants << r1
+p2.restaurants << r2
+p3.restaurants << r1
+p4.restaurants << r3
+p5.restaurants << r4
+p6.restaurants << r1

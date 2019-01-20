f3b350d7164751cb241a636b6ba26bd44b295c0f
David Pearl <davidtpearl@gmail.com>
2019-01-20 21:48:21 +1100

    Form to create a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
 
+  #skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def index
     @posts = Post.all
+  end
+
+  def show
+    @post = Post.find params[:id]
   end
 
   def new
@@ -9,8 +16,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new post_params
+    @post = Post.create post_params
+    redirect_to @post
   end
+
 
 
   private

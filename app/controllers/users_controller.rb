b6334b36b8496af1b1d4a0e77ae639c48d95a466
David Pearl <davidtpearl@gmail.com>
2019-01-22 20:16:05 +1100

    Added secure password check to user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,30 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      # session[:user_id] = @user.id
+      redirect_to user_path(@user) # Future -> root_path
+    else
+      # Send user back to the creat new user page to fix their mistakes.
+      render :new
+    end
 
   end
 
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
+
+# if params[:file].present?
+#       # Then call Cloudinary's upload method, passing in the file in params
+#       req = Cloudinary::Uploader.upload(params[:file])
+#       # Using the public_id allows us to use Cloudinary's powerful image
+#       # transformation methods.
+#       animal.image = req["public_id"]
+#       animal.save
+#     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@
 #
 # Table name: users
 #
-#  id         :bigint(8)        not null, primary key
-#  name       :string
-#  email      :string
-#  password   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  name            :string
+#  email           :string
+#  password        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
 #
 
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, :presence => true, :uniqueness => true
   has_many :posts
 end

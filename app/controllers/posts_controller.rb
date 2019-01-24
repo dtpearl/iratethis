28698a9ca15715c572863605c09b0ba5b4fb9032
David Pearl <davidtpearl@gmail.com>
2019-01-25 10:39:53 +1100

    Changed display order of posts 'posts#index' & 'users#show'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authorise_user
 
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
     @posts = Post.where :user_id => @user.id
+    @posts = @posts.reverse
   end
 
   def create

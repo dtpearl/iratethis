00fcdd90c30255c56ae6da2e273bcefdfaca43e2
David Pearl <davidtpearl@gmail.com>
2019-01-24 21:11:50 +1100

    Layouts for all pages - Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    redirect_to root_path
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root :to => 'pages#index'
+  root :to => 'session#new'
   resources :posts
   resources :comments
   resources :users
